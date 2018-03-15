2019af1662d46c1db5757fe2ba1af367be8f05b9
Alexandre Coulombe <32465816+acoulombe@users.noreply.github.com>
2018-03-14 23:21:17 -0400

    Shortened corner localization to go better with navigation step

--- DPM_FINAL_PROJECT/src/ca/mcgill/ecse211/Localization/Full_Localization.java
+++ DPM_FINAL_PROJECT/src/ca/mcgill/ecse211/Localization/Full_Localization.java
@@ -85,7 +85,7 @@ public class Full_Localization {
 		motorcontrol.forward(); 	//set the robot in motion towards x = 0 line
 		LSL.fix_angle(); 			//stop at x = 0 line
 		parameter_correction(exp_x,exp_y);
-		try{
+		/*try{
 			Thread.sleep(200);
 		}catch (Exception e){}
 		motorcontrol.moveSetDistance(LS_offset);
@@ -98,7 +98,7 @@ public class Full_Localization {
 		}catch (Exception e){}
 		motorcontrol.backward();
 		LSL.fix_angle(); 			//stops when at 0 degrees
-		parameter_correction(exp_x,exp_y);
+		parameter_correction(exp_x,exp_y);*/
 	}
 	
 	/**
