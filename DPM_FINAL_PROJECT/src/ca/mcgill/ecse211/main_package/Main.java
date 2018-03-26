ce1d01f3379e351685a319c39409dd87890d8545
HollowTube <truongtritin98@gmail.com>
2018-03-26 14:49:14 -0400

    full integration

--- DPM_FINAL_PROJECT/src/ca/mcgill/ecse211/main_package/Main.java
+++ DPM_FINAL_PROJECT/src/ca/mcgill/ecse211/main_package/Main.java
@@ -16,14 +16,14 @@ import lejos.robotics.SampleProvider;
 /**
  * Main class of the project. It is meant to conduct the entire run and manage
  * all the different threads. All the required tasks will be called upon here
- * for the robot to conduct.
- * This includes the initial localization, navigation, obstacle crossing, field localization and 
- * search and identification of flag (eventually).
- *<p>
+ * for the robot to conduct. This includes the initial localization, navigation,
+ * obstacle crossing, field localization and search and identification of flag
+ * (eventually).
+ * <p>
  * Main class, meant to conduct the entire run and manage all the different
  * threads.
  * <p>
->>>>>>> branch 'master' of https://github.com/HollowTube/dpm.git
+ * >>>>>>> branch 'master' of https://github.com/HollowTube/dpm.git
  * 
  * @author Tritin, Alexandre, Matthew
  */
@@ -37,7 +37,7 @@ public class Main {
 	private static final Port sensorPortColor = LocalEV3.get().getPort("S3");
 
 	public static final double WHEEL_RAD = 2.06;
-	public static final double TRACK = 14.46;
+	public static final double TRACK = 14.76;
 	public static final double TILE_SIZE = 30.48;
 
 	// ultrasonic sensor initialization
@@ -74,8 +74,8 @@ public class Main {
 	}
 
 	static List_of_states state;
-	
-	double xf,yf;
+
+	double xf, yf;
 
 	/**
 	 * Main method to run the program.
@@ -101,11 +101,14 @@ public class Main {
 		// simply input waypoints here, will only update after it reaches the
 		// destination
 
-		int[][] waypoints = { {parameters.Green_start_coord_x(), parameters.TN_coord_y()},
-				 			  {parameters.TN_coord_x(), parameters.TN_coord_y()}, 
-				 			  {parameters.BR_coord_x(), parameters.BR_coord_y()}, 
-				 			  {parameters.Green_start_coord_x(), parameters.TN_coord_y()}, 
-				 			  {parameters.Green_start_coord_x(), parameters.Green_start_coord_y()} };
+		int[][] waypoints = { { parameters.Green_start_coord_x(), parameters.TN_coord_y() },
+				{ parameters.TN_coord_x(), parameters.TN_coord_y() },
+				{ parameters.TN_end_x(parameters.TN_coord_x()), parameters.TN_end_y(parameters.TN_coord_y()) },
+				{ parameters.BR_coord_x(), parameters.BR_coord_y() },
+				{ parameters.BR_end_x(parameters.BR_coord_x()), parameters.BR_end_y(parameters.BR_coord_y()) },
+				{ parameters.Green_start_coord_x(), parameters.TN_coord_y() },
+				{ parameters.Green_start_coord_x(), parameters.Green_start_coord_y() } };
+		// int[][] waypoints = {{1,2},{3,2},{3,6},{5,6},{5,2}};
 		int current_waypoint = 0;
 		double xf = 0;
 		double yf = 0;
@@ -127,8 +130,8 @@ public class Main {
 		} else if (buttonChoice == Button.ID_UP) {
 			Calibration.track_calibration();
 		}
-		
-		else if(buttonChoice == Button.ID_ENTER) {
+
+		else if (buttonChoice == Button.ID_ENTER) {
 			Testing.straightLine();
 		}
 		// Start odometer and display threads
@@ -144,8 +147,6 @@ public class Main {
 			odoCorrectionThread.start();
 		}
 
-
-
 		// state machine implementation, if you add any states makes sure that it does
 		// not get stuck in a loop
 
@@ -155,30 +156,32 @@ public class Main {
 			switch (state) {
 
 			case INITIALIZE:
-				 try {
-				 Localize.Corner_Localize(parameters.Green_start_coord_x(), parameters.Green_start_coord_y());
-				 } catch (OdometerExceptions e) {
-				 // TODO Auto-generated catch block
-				 e.printStackTrace();
-				 }
-				//odometer.setXYT(0.01, 0.01, 0.01);
+				try {
+					// Localize.Corner_Localize(parameters.Green_start_coord_x(),
+					// parameters.Green_start_coord_y());
+					Localize.Corner_Localize(1, 1);
+				} catch (OdometerExceptions e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				// odometer.setXYT(0.01, 0.01, 0.01);
 				state = List_of_states.TURNING;
 				break;
 
 			// do nothing until button is pressed up
 			case IDLE:
-				
+
 				while (Button.waitForAnyPress() != Button.ID_UP)
 					sleeptime(50); // waits until the up button is pressed
-				odometer.setXYT(0.01, 0.01, 0.01);
-				state = List_of_states.BRIDGE_CROSSING;
+				odometer.setXYT(1 * TILE_SIZE, 1.01 * TILE_SIZE, 0.01);
+				state = List_of_states.TURNING;
 				break;
 			// dime turn towards necessary destination
 			case TURNING:
-				
+
 				Sound.beep();
-				xf = waypoints[current_waypoint][0];
-				yf = waypoints[current_waypoint][1];
+				xf = waypoints[current_waypoint][0] * TILE_SIZE + 0.01;
+				yf = waypoints[current_waypoint][1] * TILE_SIZE + 0.01;
 				navigator.turn_to_destination(xf, yf);
 				state = List_of_states.TRAVELLING;
 				break;
@@ -186,7 +189,7 @@ public class Main {
 			// travels to waypoints while scanning for objects
 			case TRAVELLING:
 
-				//navigator.travelTo(xf, yf);
+				// navigator.travelTo(xf, yf);
 				motorControl.forward();
 				A_loc.fix_angle_on_path();
 
@@ -200,76 +203,77 @@ public class Main {
 						current_waypoint = 0;
 						state = List_of_states.IDLE;
 
-					}
-					else if(current_waypoint == 2){
+					} else if (current_waypoint == 2) {
 						state = List_of_states.TUNNEL_CROSSING;
-					}
-					else if(current_waypoint == 3){
+					} else if (current_waypoint == 4) {
 						state = List_of_states.BRIDGE_CROSSING;
-					}
-					else {
+					} else {
 						state = List_of_states.TURNING;
 					}
 					break;
 				}
 				break;
-			
+
 			case TILE_LOCALIZATION:
 				try {
-					 Localize.Tile_Localize(1,1);
-					} catch (OdometerExceptions e) {
-					 e.printStackTrace();
-					}
-				state =List_of_states.IDLE;
+					Localize.Tile_Localize(1, 1);
+				} catch (OdometerExceptions e) {
+					e.printStackTrace();
+				}
+				state = List_of_states.IDLE;
 				break;
-				
+
 			case IDENTIFYING:
-				
+
 				state = List_of_states.IDLE;
 				break;
 
 			case BRIDGE_CROSSING:
-				
-				xf = waypoints[current_waypoint][0]*TILE_SIZE;
-				yf = waypoints[current_waypoint][1]* TILE_SIZE;
-				
-				navigator.offset90(xf,yf);
-				motorControl.moveSetDistance(14.3);
-				motorControl.dimeTurn(87);
-				motorControl.moveSetDistance(100);
+
+				xf = waypoints[current_waypoint][0];
+				yf = waypoints[current_waypoint][1];
+
+				navigator.offset90(xf * TILE_SIZE, yf * TILE_SIZE);
+				motorControl.moveSetDistance(13.5);
+				motorControl.dimeTurn(88);
+				motorControl.moveSetDistance(110);
 				try {
-					Localize.Tile_Localize(parameters.BR_end_x(parameters.BR_coord_x()), parameters.BR_end_y(parameters.BR_coord_y()));
+					Localize.Tile_Localize((int) xf, (int) yf);
 				} catch (OdometerExceptions e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-					
+
 				}
+				current_waypoint++;
 				state = List_of_states.TURNING;
 				break;
-				
+
 			case TUNNEL_CROSSING:
-				double bridge_length = 60;
-				navigator.offset90(0,30);
-				motorControl.moveSetDistance(15);
+
+				xf = waypoints[current_waypoint][0];
+				yf = waypoints[current_waypoint][1];
+				navigator.offset90(xf * TILE_SIZE, yf * TILE_SIZE);
+				motorControl.moveSetDistance(14);
 				motorControl.dimeTurn(90);
-				motorControl.moveSetDistance(bridge_length + 40);
+				motorControl.moveSetDistance(110);
 				try {
-					Localize.Tile_Localize(parameters.TN_end_x(parameters.TN_coord_x()), parameters.TN_end_y(parameters.TN_coord_y()));
+					Localize.Tile_Localize((int) xf, (int) yf);
+
 				} catch (OdometerExceptions e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
-					
+
 				}
+				current_waypoint++;
 				state = List_of_states.TURNING;
 				break;
-				
 
 			case TEST:
 				motorControl.forward();
-//				while(!lightPollerleft.lessThan(30));
-//				motorControl.stop();
-			
-//				state = List_of_states.IDLE;
+				// while(!lightPollerleft.lessThan(30));
+				// motorControl.stop();
+
+				// state = List_of_states.IDLE;
 				break;
 			default:
 				break;
@@ -282,7 +286,8 @@ public class Main {
 	/**
 	 * This method sets a time for thread sleep.
 	 * 
-	 * @param time sleep time
+	 * @param time
+	 *            sleep time
 	 */
 	public static void sleeptime(int time) {
 		try {
--- DPM_FINAL_PROJECT/src/ca/mcgill/ecse211/main_package/Parameter_intake.java
+++ DPM_FINAL_PROJECT/src/ca/mcgill/ecse211/main_package/Parameter_intake.java
@@ -11,23 +11,23 @@ public class Parameter_intake {
 	public int RedTeam; 		//Team starting from red zone
 	public int GreenTeam = 13;   	//Team starting from green zone
 	public int RedCorner;		//Starting corner for red team
-	public int GreenCorner;		//Starting corner for green team
+	public int GreenCorner = 1;		//Starting corner for green team
 	public int OG;				//color of green opponent flag
 	public int OR;				//color of red opponent flag
-	public int Red_LL_x = 6;		//x coordinate of lower left hand corner of Red Zone
-	public int Red_LL_y = 0;		//y coordinate of lower left hand corner of Red Zone
+	public int Red_LL_x = 0;		//x coordinate of lower left hand corner of Red Zone
+	public int Red_LL_y = 5;		//y coordinate of lower left hand corner of Red Zone
 	public int Red_UR_x = 8;		//x coordinate of upper right hand corner of Red Zone
-	public int Red_UR_y = 7;		//y coordinate of upper right hand corner of Red Zone
+	public int Red_UR_y = 8;		//y coordinate of upper right hand corner of Red Zone
 	public int Green_LL_x = 0;		//x coordinate of lower left hand corner of Green Zone
 	public int Green_LL_y = 0;		//y coordinate of lower left hand corner of Green Zone
-	public int Green_UR_x = 2;		//x coordinate of upper right hand corner of Green Zone
-	public int Green_UR_y = 5;		//y coordinate of upper right hand corner of Green Zone
+	public int Green_UR_x = 8;		//x coordinate of upper right hand corner of Green Zone
+	public int Green_UR_y = 3;		//y coordinate of upper right hand corner of Green Zone
 	public int TN_LL_x = 2;			//x coordinate of lower left corner of the tunnel footprint
-	public int TN_LL_y = 2;			//y coordinate of lower left corner of the tunnel footprint
-	public int TN_UR_x = 6;			//x coordinate of upper right corner of the tunnel footprint
-	public int TN_UR_y = 3;			//y coordinate of upper right corner of the tunnel footprint
-	public int BR_LL_x = 2;			//x coordinate of lower left corner of the bridge footprint
-	public int BR_LL_y = 4;			//y coordinate of lower left corner of the bridge footprint
+	public int TN_LL_y = 3;			//y coordinate of lower left corner of the tunnel footprint
+	public int TN_UR_x = 3;			//x coordinate of upper right corner of the tunnel footprint
+	public int TN_UR_y = 5;			//y coordinate of upper right corner of the tunnel footprint
+	public int BR_LL_x = 5;			//x coordinate of lower left corner of the bridge footprint
+	public int BR_LL_y = 3;			//y coordinate of lower left corner of the bridge footprint
 	public int BR_UR_x = 6;			//x coordinate of upper right corner of the bridge footprint
 	public int BR_UR_y = 5;			//y coordinate of upper right corner of the bridge footprint
 	public int SR_LL_x;			//x coordinate of lower left corner of the search region in red player zone
